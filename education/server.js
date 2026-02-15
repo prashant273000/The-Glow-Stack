d54f7d88ed61316f6c9a3a144f3c3051dc7beda3
Prashant Prabhakar <prashant273000@gmail.com>
2026-02-15 19:20:28 +0530

    Fixed-env-file

--- education/server.js
+++ education/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const Groq = require("groq-sdk"); // Integrated from second code
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
@@ -12,9 +13,9 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 // --- 1. CONFIGURATION ---
-const GEMINI_KEY = "AIzaSyA03Yt0CASsuC-LSuQwIO6ruSRsErf8sio"; 
-const GROQ_KEY = "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"; 
-const TRANSCRIPT_API_KEY = "sk_lgyipcog-61owjeWsw7KfpFRoI1EBWetTPbdLrsqZ5c";
+const GEMINI_KEY = process.env.GEMINI_KEY 
+const GROQ_KEY = process.env.GROQ_KEY; 
+const TRANSCRIPT_API_KEY = process.env.TRANSCRIPT_API_KEY;
 
 const genAI = new GoogleGenerativeAI(GEMINI_KEY);
 const groq = new Groq({ apiKey: GROQ_KEY }); // Initialized Groq
